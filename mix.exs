5fbed374531ac0c49e626c1c3ee2cf16efc670b2
Cam Cook <cam.cook.codes@gmail.com>
2025-05-06 13:40:28 -0400

    chore: setup

--- mix.exs
+++ mix.exs
@@ -1,13 +1,61 @@
 defmodule Ftp2Cloud.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/camatcode/ftp_2_cloud"
+  @version "0.9.0"
+
   def project do
     [
       app: :ftp_2_cloud,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      package: package(),
+      description: """
+      Adds an FTP interface to your cloud service provider
+      """,
+
+      # Docs
+      name: "FTP2Cloud",
+      docs: [
+        main: "FTP2Cloud",
+        api_reference: false,
+        logo: "assets/ftp_2_cloud-logo.png",
+        source_ref: "v#{@version}",
+        source_url: @source_url,
+        extra_section: "GUIDES",
+        extras: extras(),
+        formatters: ["html"],
+        extras: extras(),
+        groups_for_modules: groups_for_modules(),
+        skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
+      ]
+    ]
+  end
+
+  defp groups_for_modules do
+    []
+  end
+
+  def package do
+    [
+      maintainers: ["Cam Cook"],
+      licenses: ["Apache-2.0"],
+      files: ~w(lib .formatter.exs mix.exs README* CHANGELOG* LICENSE*),
+      links: %{
+        Website: @source_url,
+        Changelog: "#{@source_url}/blob/master/CHANGELOG.md",
+        GitHub: @source_url
+      }
+    ]
+  end
+
+  def extras do
+    [
+      "README.md"
     ]
   end
 
@@ -21,8 +69,12 @@ defmodule Ftp2Cloud.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.37", only: :dev, runtime: false},
+      {:ex_license, "~> 0.1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_machina, "~> 2.8.0", only: :test},
+      {:faker, "~> 0.18.0", only: :test},
+      {:req, "~> 0.5.10"}
     ]
   end
 end
--- test/ftp2_cloud_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Ftp2CloudTest do
-  use ExUnit.Case
-  doctest Ftp2Cloud
-
-  test "greets the world" do
-    assert Ftp2Cloud.hello() == :world
-  end
-end
