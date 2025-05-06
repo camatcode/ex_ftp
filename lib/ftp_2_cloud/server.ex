7fef0a5dee317fdfa72318258083fa519469cff2
Cam Cook <cam.cook.codes@gmail.com>
2025-05-06 14:57:53 -0400

    build: setup app; removed PROPIN

---
+++ lib/ftp_2_cloud/server.ex
@@ -0,0 +1,47 @@
+defmodule StudioFtp.Server do
+  @moduledoc false
+
+  use GenServer
+  require Logger
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts)
+  end
+
+  def init(opts) do
+    port = Keyword.get(opts, :port, 4040)
+
+    {:ok, socket} =
+      :gen_tcp.listen(
+        port,
+        [:binary, packet: :line, active: true, reuseaddr: true]
+      )
+
+    Logger.info("Accepting connections on port #{port}")
+
+    {:ok, %{socket: socket}, {:continue, :accept}}
+  end
+
+  def handle_continue(:accept, %{socket: socket} = state) do
+    accept(socket)
+
+    {:noreply, state}
+  end
+
+  def handle_info(:accept, %{socket: socket} = state) do
+    accept(socket)
+
+    {:noreply, state}
+  end
+
+  defp accept(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+
+    {:ok, pid} =
+      DynamicSupervisor.start_child(FTP2Cloud.WorkerSupervisor, {FTP2Cloud.Worker, client})
+
+    :ok = :gen_tcp.controlling_process(client, pid)
+
+    send(self(), :accept)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -62,7 +62,8 @@ defmodule Ftp2Cloud.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Ftp2Cloud.Application, []}
     ]
   end
 
