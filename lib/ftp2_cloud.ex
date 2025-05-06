50381715be5f9ad611e953a81421319331356f1b
Cam Cook <cam.cook.codes@gmail.com>
2025-05-06 13:04:08 -0400

    chore: first

---
+++ lib/ftp2_cloud.ex
@@ -0,0 +1,18 @@
+defmodule Ftp2Cloud do
+  @moduledoc """
+  Documentation for `Ftp2Cloud`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ftp2Cloud.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
