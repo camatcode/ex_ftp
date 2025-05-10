60c6abe279f591a3c6a38eff92028caecbe6fa35
Cam Cook <cam.cook.codes@gmail.com>
2025-05-10 14:55:18 -0400

    docs: Worker, Server, Passive Socket, FTP Common, Connector Common

--- lib/ex_ftp/worker.ex
+++ lib/ex_ftp/worker.ex
@@ -5,6 +5,7 @@ defmodule ExFTP.Worker do
 
   use GenServer
 
+  import Bitwise
   import ExFTP.Common
   import ExFTP.Connector.Common
 
@@ -356,5 +357,38 @@ defmodule ExFTP.Worker do
 
   defp noreply(state) do
     {:noreply, state}
+  end
+
+  defp ip_port_to_pasv(ip, port) do
+    upper_port = port >>> 8
+    lower_port = port &&& 255
+    {a, b, c, d} = ip
+    # Convert IP and port (e.g. 64943) to (192,168,1,22,253,175)
+    "#{a},#{b},#{c},#{d},#{upper_port},#{lower_port}"
+  end
+
+  defp with_pasv_socket(%{pasv_socket: pasv} = state) do
+    if pasv && Process.alive?(pasv) do
+      {:ok, pasv}
+    else
+      :ok = send_resp(550, "LIST failed. PASV mode required.", Map.get(state, :socket))
+      {:noreply, state}
+    end
+  end
+
+  defp quit(%{socket: socket} = state) do
+    Logger.info("Shutting down. Client closed connection.")
+
+    :ok = send_resp(221, "Closing connection.", socket)
+
+    :gen_tcp.close(socket)
+
+    pasv = state[:pasv_socket]
+
+    if pasv && Process.alive?(pasv) do
+      PassiveSocket.close(pasv)
+    end
+
+    {:stop, :normal, state}
   end
 end
