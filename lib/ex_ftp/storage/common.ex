848a0906bf08f3db93ebe4d99fd5c1f09697cd77
Cam Cook <cam.cook.codes@gmail.com>
2025-05-12 16:19:47 -0400

    test: FTP Common

--- lib/ex_ftp/storage/common.ex
+++ lib/ex_ftp/storage/common.ex
@@ -16,23 +16,6 @@ defmodule ExFTP.Storage.Common do
 
   alias ExFTP.PassiveSocket
 
-  @dummy_directories [
-    %{
-      file_name: ".",
-      size: 4096,
-      type: :directory,
-      access: :read_write,
-      modified_datetime: DateTime.now!("Etc/UTC")
-    },
-    %{
-      file_name: "..",
-      size: 4096,
-      type: :directory,
-      access: :read_write,
-      modified_datetime: DateTime.now!("Etc/UTC")
-    }
-  ]
-
   @directory_action_ok 257
   @directory_action_not_taken 521
   @file_action_ok 250
@@ -292,13 +275,14 @@ defmodule ExFTP.Storage.Common do
     send_resp(@opening_data_connection, "Here comes the directory listing.", socket)
 
     wd = change_prefix(connector.get_working_directory(connector_state), path)
+    hidden_dirs = if include_hidden, do: get_hidden_roots(connector, connector_state), else: []
 
     items =
       connector.get_directory_contents(wd, connector_state)
       |> case do
         {:ok, contents} ->
           if include_hidden do
-            @dummy_directories ++ contents
+            hidden_dirs ++ contents
           else
             contents
             |> Enum.reject(&hidden?/1)
@@ -306,7 +290,7 @@ defmodule ExFTP.Storage.Common do
           |> Enum.sort_by(& &1.file_name)
 
         _ ->
-          if include_hidden, do: @dummy_directories, else: []
+          if include_hidden, do: hidden_dirs, else: []
       end
       |> Enum.map(&format_content(&1))
 
@@ -371,12 +355,14 @@ defmodule ExFTP.Storage.Common do
 
     wd = change_prefix(connector.get_working_directory(connector_state), path)
 
+    hidden_dirs = if include_hidden, do: get_hidden_roots(connector, connector_state), else: []
+
     items =
       connector.get_directory_contents(wd, connector_state)
       |> case do
         {:ok, contents} ->
           if include_hidden do
-            @dummy_directories ++ contents
+            hidden_dirs ++ contents
           else
             contents
             |> Enum.reject(&hidden?/1)
@@ -384,7 +370,7 @@ defmodule ExFTP.Storage.Common do
           |> Enum.sort_by(& &1.file_name)
 
         _ ->
-          if include_hidden, do: @dummy_directories, else: []
+          if include_hidden, do: hidden_dirs, else: []
       end
       |> Enum.map(&format_name(&1))
 
@@ -665,5 +651,14 @@ defmodule ExFTP.Storage.Common do
         chunk -> {:cont, chunk, []}
       end
     )
+  end
+
+  defp get_hidden_roots(connector, connector_state) do
+    w_path = change_prefix(connector.get_working_directory(connector_state), ".")
+
+    {:ok, first} = connector.get_content_info(w_path, connector_state)
+    w_path = change_prefix(connector.get_working_directory(connector_state), "..")
+    {:ok, second} = connector.get_content_info(w_path, connector_state)
+    [first, second]
   end
 end
