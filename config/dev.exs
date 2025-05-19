e55a0fa2d27f384810d43d9d5d9db494fc9e7d76
Cam Cook <cam.cook.codes@gmail.com>
2025-05-19 14:30:46 -0400

    docs: config changes

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ import Config
 config :ex_aws,
   s3: [
     scheme: "http://",
-    host: "ariadne",
+    host: System.get_env("AWS_HOST"),
     port: 4566,
     access_key_id: "",
     secret_access_key: ""
