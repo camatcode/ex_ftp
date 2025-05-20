1866e676aafb39148b0c6b8d6ac21283ca461a17
Cam Cook <cam.cook.codes@gmail.com>
2025-05-20 19:29:52 -0400

    ci: environment var defaults

--- config/dev.exs
+++ config/dev.exs
@@ -2,9 +2,9 @@ import Config
 
 config :ex_aws,
   s3: [
-    scheme: "http://",
-    host: System.get_env("AWS_HOST"),
-    port: 4566,
+    scheme: System.get_env("AWS_SCHEME", "http://"),
+    host: System.get_env("AWS_HOST", "localhost"),
+    port: "AWS_PORT" |> System.get_env("4566") |> String.to_integer(),
     access_key_id: "",
     secret_access_key: ""
   ]
--- config/test.exs
+++ config/test.exs
@@ -3,14 +3,15 @@ import Config
 config :ex_aws,
   s3: [
     scheme: System.get_env("AWS_SCHEME", "http://"),
-    host: System.get_env("AWS_HOST"),
+    host: System.get_env("AWS_HOST", "localhost"),
     port: "AWS_PORT" |> System.get_env("4566") |> String.to_integer(),
     access_key_id: "",
     secret_access_key: ""
   ]
 
 config :ex_ftp,
-  ftp_port: 4041,
+  ftp_port: "FTP_PORT" |> System.get_env("4041") |> String.to_integer(),
+  ftp_addr: System.get_env("FTP_ADDR", "127.0.0.1"),
   min_passive_port: "MIN_PASSIVE_PORT" |> System.get_env("40002") |> String.to_integer(),
   max_passive_port: "MAX_PASSIVE_PORT" |> System.get_env("40007") |> String.to_integer(),
   authenticator: ExFTP.Auth.PassthroughAuth,
