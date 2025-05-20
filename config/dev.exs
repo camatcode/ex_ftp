e722b31e75260635499d6a4af5448901f78e34dc
Cam Cook <cam.cook.codes@gmail.com>
2025-05-19 23:09:27 -0400

    fix: tests

--- config/dev.exs
+++ config/dev.exs
@@ -8,3 +8,11 @@ config :ex_aws,
     access_key_id: "",
     secret_access_key: ""
   ]
+
+config :ex_ftp,
+  ftp_port: "FTP_PORT" |> System.get_env("4040") |> String.to_integer(),
+  min_passive_port: "MIN_PASSIVE_PORT" |> System.get_env("40002") |> String.to_integer(),
+  max_passive_port: "MAX_PASSIVE_PORT" |> System.get_env("40007") |> String.to_integer(),
+  authenticator: ExFTP.Auth.PassthroughAuth,
+  storage_config: %{storage_bucket: "ex-ftp-test"},
+  storage_connector: ExFTP.Storage.S3Connector
