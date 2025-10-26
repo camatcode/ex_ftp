0a4a1d8e1c6222b638efdaa0cc88dc2766a60738
Cam Cook <cam.cook.codes@gmail.com>
2025-10-26 04:24:17 -0400

    build: v1.0.5

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule ExFTP.MixProject do
   alias ExFTP.Storage.S3ConnectorConfig
 
   @source_url "https://github.com/camatcode/ex_ftp"
-  @version "1.0.4"
+  @version "1.0.5"
 
   def project do
     [
