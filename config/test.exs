cbe4a873a8575e82cdac96dfe00025aaea8c7f8c
Cam Cook <cam.cook.codes@gmail.com>
2025-12-07 12:17:17 -0500

    fix: revert to use LocalStack (which is what the ci uses)

--- config/test.exs
+++ config/test.exs
@@ -7,9 +7,9 @@ config :ex_aws,
   s3: [
     scheme: System.get_env("AWS_SCHEME", "http://"),
     host: System.get_env("AWS_HOST", "localhost"),
-    port: "AWS_PORT" |> System.get_env("9222") |> String.to_integer(),
-    access_key_id: "minio",
-    secret_access_key: "minio123456"
+    port: "AWS_PORT" |> System.get_env("4566") |> String.to_integer(),
+    access_key_id: "",
+    secret_access_key: ""
   ]
 
 config :ex_ftp,
